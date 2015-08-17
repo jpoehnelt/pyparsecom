8c033542b52afe720b044b3ce7de56681abed731
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-08-17 00:16:26 -0500

    parse doesn't always return a code

--- pyparsecom/exceptions.py
+++ pyparsecom/exceptions.py
@@ -23,9 +23,10 @@ class ParseError(Exception):
     def __init__(self, **kwargs):
         self.code = kwargs.get('code')
         self.error = kwargs.get('error')
-
-        message = '%s %d' % (self.error, self.code)
-
+        try:
+            message = '%s %d' % (self.error, self.code)
+        except:
+            message = str(kwargs)
         super(ParseError, self).__init__(message)
 
 class ParseResourceException(Exception):
